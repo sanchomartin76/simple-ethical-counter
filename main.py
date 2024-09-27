4b4b9e91fff0dd81901758d2b61ba9cc4de63d44
sanchomartin76 <144518730+sanchomartin76@users.noreply.github.com>
2024-09-27 20:44:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def main(page: ft.Page):
         page.update()
 
     def plus_click(e):
-        #test.hello_world()
+        hello_world()
         txt_number.value = str(int(txt_number.value) + 3)
         page.update()
 
